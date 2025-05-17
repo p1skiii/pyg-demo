5231ccccc7a5df858a1dbe8ade07ea5a083aea9a
P1ski <cheerstt1@gmail.com>
2025-05-18 01:28:43 +0800

    完善get_tensor方法，支持多种索引类型；新增测试文件test_get_tensor.py以验证功能

--- toy_feature_store.py
+++ toy_feature_store.py
@@ -23,9 +23,20 @@ class ToyFeatureStore:
             raise ValueError("Index must be an int or a list of ints")
         return self
     
-    def get_tensor(self, group_name, after_name, index):
-        key = (group_name, after_name)
-        return self._store[key][index]
+    def get_tensor(self, group_name, attr_name, index):
+        key = (group_name, attr_name)
+        if isinstance(index, int):
+            return self._store[key][index]
+        elif isinstance(index, (list, torch.Tensor)):
+            return torch.stack([self._store[key][int(i)] for i in index])
+        elif isinstance(index, slice):
+            indices = range(index.start or 0, index.stop, index.step or 1)
+            return torch.stack([self._store[key][i] for i in indices])
+        elif index is None:
+            all_indices = sorted(self._store[key].keys())
+            return torch.stack([self._store[key][i] for i in all_indices])
+        else:
+        raise TypeError("不支持的 index 类型")
 
     def __getitem__(self, key):
         group_name, after_name, index = key
