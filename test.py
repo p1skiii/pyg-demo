1117db2b1c4a90deb2c4df3753903baeec555b94
P1ski <cheerstt1@gmail.com>
2025-05-17 22:56:52 +0800

    Start

--- test.py
+++ test.py
@@ -1,13 +1,28 @@
+from toy_feature_store import ToyFeatureStore
 import torch
-from torch_geometric.data import Data
+import pandas as pd
 
-print(f"[成功] PyTorch版本: {torch.__version__}")
-print(f"[加速] MPS可用: {torch.backends.mps.is_available()}")
 
-# 创建示例图数据
-edge_index = torch.tensor([[0, 1], [1, 2]], device='mps')  # 直接放到MPS设备
-x = torch.tensor([[1], [2], [3]], device='mps', dtype=torch.float)
+fs = ToyFeatureStore()
 
-data = Data(x=x, edge_index=edge_index.t().contiguous())
-print(f"[数据] 示例图数据:\n{data}")
+# 存入节点0的'x'特征
+fs.put_tensor(torch.tensor([1.0, 2.0]), 'paper', 'x', 0)
+# 存入节点1的'x'特征
+fs.put_tensor(torch.tensor([3.0, 4.0]), 'paper', 'x', 1)
+# 存入节点0的'y'特征
+fs.put_tensor(torch.tensor([1]), 'paper', 'y', 0)
+
+# 取出节点0的'x'特征
+x0 = fs.get_tensor('paper', 'x', 0)
+print("节点0的x特征：", x0)
+x0 = fs['paper', 'x', 0]
+print("节点0的x特征：", x0)
+
+# 取出节点1的'x'特征
+x1 = fs.get_tensor('paper', 'x', 1)
+print("节点1的x特征：", x1)
+
+# 取出节点0的'y'特征
+y0 = fs.get_tensor('paper', 'y', 0)
+print("节点0的y特征：", y0)
 
---
+++ toy_feature_store.py
@@ -0,0 +1,25 @@
+import torch
+import numpy as np
+import pandas as pd
+
+class ToyFeatureStore:
+    def __init__(self):
+        self._store = {}
+
+    def put_tensor(self, tensor, group_name, after_name, index):
+        key = (group_name, after_name)
+
+        if key not in self._store:
+            self._store[key] = {}
+        self._store[key][index] = tensor
+        return self
+    
+    def get_tensor(self, group_name, after_name, index):
+        key = (group_name, after_name)
+        return self._store[key][index]
+
+    def __getitem__(self, key):
+        group_name, after_name, index = key
+        return self.get_tensor(group_name, after_name, index)
+
+    
